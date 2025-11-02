0d8c8e1d86ecdef8e1467774c73464bc58eadd20
The Heoster <90freeplay98@gmail.com>
2025-11-02 15:04:58 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "codeex-ai-new-project",
-  "appId": "1:852700164485:web:36b1351f873b2c93816e83",
-  "apiKey": "AIzaSyCVnF_5Jz3S5E1-q7E9Yj_j1b3E2y8D8Yw",
-  "authDomain": "codeex-ai-new-project.firebaseapp.com",
+  "appId": "1:852700164485:web:5429959227ffb3d58794ec",
   "storageBucket": "codeex-ai-new-project.appspot.com",
+  "apiKey": "AIzaSyDNeBOCkQ6afGGuL4XUrBdmSdGWLK37uxk",
+  "authDomain": "codeex-ai-new-project.firebaseapp.com",
   "messagingSenderId": "852700164485"
 };
