066376bbb82cb930a6a94898be1fa09b4e88c914
The Heoster <90freeplay98@gmail.com>
2025-11-02 09:04:00 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,14 @@
     pkgs.zulu
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { NEXT_PUBLIC_FIREBASE_API_KEY = "AIzaSyA53FGtCMKPFS50rl-egSZMeVYJ_RhEhsU";
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "codeex-ai-v3.firebaseapp.com";
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "codeex-ai-v3";
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = "codeex-ai-v3.firebasestorage.app";
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "180880740597";
+    NEXT_PUBLIC_FIREBASE_APP_ID = "1:180880740597:web:6397845be218ba33b7f5bc";
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID = "G-ZQQRX5CZ1R";
+  };
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
